7067d64ba0cea6c829527f37741cb9c7ec8935cb
chengweiou <chengwei.ou.dora@gmail.com>
2021-06-15 13:20:12 -0700

    add jwt rsa

--- src/main/java/chengweiou/universe/wormhole/base/jwt/JwtConfig.java
+++ src/main/java/chengweiou/universe/wormhole/base/jwt/JwtConfig.java
@@ -1,5 +1,4 @@
 package chengweiou.universe.wormhole.base.jwt;
-// package chengweiou.universe.andromeda.base.jwt;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -13,7 +12,12 @@ public class JwtConfig {
     private String issuer;
     // token，过期了无法请求
     private Long expMinute;
+    // 2 选 1
+    // sign 用于 hs 加密
     private String sign;
+    // rsa 用于 rs 加密
+    private String rsaPublicPath;
+    private String rsaPrivatePath;
     // refresh token，用来获得新的token
     private Long refreshExpMinute;
 }
--- src/main/java/chengweiou/universe/wormhole/base/jwt/JwtUtil.java
+++ src/main/java/chengweiou/universe/wormhole/base/jwt/JwtUtil.java
@@ -1,9 +1,14 @@
 package chengweiou.universe.wormhole.base.jwt;
 
+import java.io.IOException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+
+import javax.annotation.PostConstruct;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -23,16 +28,21 @@ import chengweiou.universe.blackhole.util.LogUtil;
 public class JwtUtil {
     @Autowired
     private JwtConfig config;
+
+
     public String sign(Account account) {
+        Algorithm algorithm = useRsa ? Algorithm.RSA256(rsaPublicKey, rsaPrivateKey) : Algorithm.HMAC512(config.getSign());
+        return sign(account, algorithm);
+    }
+    private String sign(Account account, Algorithm algorithm) {
         try {
             Date expiresAt = Date.from(LocalDateTime.now(ZoneId.of("UTC")).plus(config.getExpMinute(), ChronoUnit.MINUTES).atZone(ZoneId.of("UTC")).toInstant());
             return JWT.create()
-                    .withIssuer(config.getIssuer())
-                    .withClaim("accountId", account.getId())
-                    .withClaim("personId", account.getPerson().getId())
-                    .withClaim("extra", account.getExtra())
-                    .withExpiresAt(expiresAt)
-                    .sign(Algorithm.HMAC512(config.getSign()));
+                .withIssuer(config.getIssuer())
+                .withClaim("personId", account.getPerson().getId())
+                .withClaim("extra", account.getExtra())
+                .withExpiresAt(expiresAt)
+                .sign(algorithm);
         } catch (JWTCreationException exception){
             //Invalid Signing configuration / Couldn't convert Claims.
             LogUtil.e("fail to sign jwt", exception);
@@ -41,20 +51,44 @@ public class JwtUtil {
     }
 
     public Account verify(String token) throws UnauthException {
+        Algorithm algorithm = useRsa ? Algorithm.RSA256(rsaPublicKey, rsaPrivateKey) : Algorithm.HMAC512(config.getSign());
+        return verify(token, algorithm);
+    }
+    private Account verify(String token, Algorithm algorithm) throws UnauthException {
         try {
-            Algorithm algorithm = Algorithm.HMAC512(config.getSign());
             JWTVerifier verifier = JWT.require(algorithm)
-                    .withIssuer(config.getIssuer())
-                    .build(); //Reusable verifier instance
+                .withIssuer(config.getIssuer())
+                .build(); //Reusable verifier instance
             DecodedJWT jwt = verifier.verify(token);
             return Builder
-                    .set("id", jwt.getClaim("accountId").asLong())
                     .set("person", Builder.set("id", jwt.getClaim("personId").asString()).to(new Person()))
                     .set("extra", jwt.getClaim("extra").asString())
                     .to(new Account());
         } catch (JWTVerificationException exception){
             //Invalid signature/claims
             throw new UnauthException();
+        }
+    }
+
+    private boolean useRsa;
+    private RSAPublicKey rsaPublicKey;
+    private RSAPrivateKey rsaPrivateKey;
+    /**
+     * now using rsa generate from https://www.devglan.com/online-tools/rsa-encryption-decryption
+     * other website generate is not working, maybe because of the format
+     * By default, the private key is generated in PKCS#8 format and the public key is generated in X.509 format.
+     */
+    @PostConstruct
+    public void init() throws IOException {
+        useRsa = !config.getRsaPublicPath().isBlank() && !config.getRsaPrivatePath().isBlank();
+        if (useRsa) {
+            try { 
+                rsaPublicKey = (RSAPublicKey) JwtPemUtils.readPublicKeyFromFile(config.getRsaPublicPath(), "RSA");
+                rsaPrivateKey = (RSAPrivateKey) JwtPemUtils.readPrivateKeyFromFile(config.getRsaPrivatePath(), "RSA");
+            } catch (IOException exception) {
+                LogUtil.e("fail to read jwt public | private key file.", exception);
+                throw exception;
+            }
         }
     }
 }
