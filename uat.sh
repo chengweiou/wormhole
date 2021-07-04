adafe2663497b6a54b4ffe286b024a2491349f2c
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-07-03 19:48:06 -0700

    add jwt pem to uat

--- uat.sh
+++ uat.sh
@@ -3,6 +3,8 @@ cp build/libs/wormhole-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universe/wormhole/ser
 #cp src/main/resources/application.yml ~/Desktop/docker/universe/wormhole/config/
 cp src/main/resources/application-uat.yml ~/Desktop/docker/universe/wormhole/config/
 cp src/main/resources/log4j2.xml ~/Desktop/docker/universe/wormhole/config/
+cp src/main/resources/jwt-public.pem ~/Desktop/docker/universe/carina/config/
+cp src/main/resources/jwt-private.pem ~/Desktop/docker/universe/carina/config/
 cp docker-compose.yml ~/Desktop/docker/universe/wormhole/docker-compose.yml
 cd ~/Desktop/docker/universe/wormhole
 
