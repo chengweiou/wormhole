be2cad7f68b7fce42b2d45bb34a80362b219f56f
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-10-05 09:56:03 -0700

    change uat xml to yml

--- uat.sh
+++ uat.sh
@@ -2,7 +2,7 @@
 cp build/libs/wormhole-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universe/wormhole/ser.jar
 #cp src/main/resources/application.yml ~/Desktop/docker/universe/wormhole/config/
 cp src/main/resources/application-uat.yml ~/Desktop/docker/universe/wormhole/config/
-cp src/main/resources/log4j2.xml ~/Desktop/docker/universe/wormhole/config/
+cp src/main/resources/log4j2.yml ~/Desktop/docker/universe/wormhole/config/
 cp src/main/resources/jwt-public.pem ~/Desktop/docker/universe/wormhole/config/
 cp src/main/resources/jwt-private.pem ~/Desktop/docker/universe/wormhole/config/
 cp docker-compose.yml ~/Desktop/docker/universe/wormhole/docker-compose.yml
