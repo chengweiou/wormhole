ff32cb68e53ac6a06f2e4daf4ec03c9c0189c868
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-11-11 22:53:42 -0800

    add config

---
+++ src/main/java/chengweiou/universe/wormhole/base/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package chengweiou.universe.wormhole.base.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+// todo tip: 使用时开启
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("async-");
+        executor.initialize();
+        return executor;
+    }
+}
---
+++ src/main/java/chengweiou/universe/wormhole/base/config/ScheduleConfig.java
@@ -0,0 +1,10 @@
+package chengweiou.universe.wormhole.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+// todo tip: 使用时开启
+@Configuration
+// @EnableScheduling
+public class ScheduleConfig {
+}
