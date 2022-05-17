18c75e53f8e9dc964129ca90b22f650d23f3a5c3
chengwei ou <chengwei.ou.dora@gmail.com>
2022-05-17 15:45:11 -0700

    upgrade blackhole

--- src/main/java/chengweiou/universe/wormhole/service/reqrecord/ReqRecordDio.java
+++ src/main/java/chengweiou/universe/wormhole/service/reqrecord/ReqRecordDio.java
@@ -5,21 +5,23 @@ import org.springframework.stereotype.Component;
 
 import chengweiou.universe.blackhole.dao.BaseDio;
 import chengweiou.universe.blackhole.dao.BaseSQL;
+import chengweiou.universe.blackhole.dao.DioCache;
+import chengweiou.universe.blackhole.dao.DioDefaultSort;
 import chengweiou.universe.blackhole.model.AbstractSearchCondition;
 import chengweiou.universe.wormhole.dao.ReqRecordDao;
 import chengweiou.universe.wormhole.model.entity.ReqRecord;
 import chengweiou.universe.wormhole.model.entity.ReqRecord.Dto;
 
+@DioCache(false)
 @Component
 public class ReqRecordDio extends BaseDio<ReqRecord, ReqRecord.Dto> {
     @Autowired
     private ReqRecordDao dao;
     @Override
     protected ReqRecordDao getDao() { return dao; }
-    @Override
-    protected Class getTClass() { return ReqRecord.class; };
-    @Override
-    protected String getDefaultSort() { return "createAt"; };
+    @DioDefaultSort("createAt")
+    private String defaultSort;
+
     @Override
     protected String baseFind(AbstractSearchCondition searchCondition, Dto sample) {
         return new BaseSQL() {{
